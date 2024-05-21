dd6018c3b99d05899ea63f54dc456ad062d9e157
Nathan Paul <TurdEye@MacBook-Pro-3.local>
2024-05-21 18:07:32 -0400

    about page/faq

--- app/dashboard/about/page.tsx
+++ app/dashboard/about/page.tsx
@@ -1,9 +1,11 @@
+import Faq from '@/app/ui/about/faq';
 import { OneIcon, ThreeIcon, TwoIcon } from '@/app/ui/components';
+import Image from 'next/image';
 
 export default function Page() {
   return (
-    <div className="flex max-h-[90vh] max-w-[1100px] flex-col overflow-scroll px-8 py-8 text-blue-600">
-      <div className="flex space-x-8 ">
+    <div className="relative flex max-h-[90vh] max-w-[1100px] flex-col overflow-scroll px-8 py-8 text-blue-600">
+      <div className="z-10 flex space-x-8 ">
         <div className="bg-highlight-green mb-8 rounded-xl p-8">
           <h2 className="mb-4 text-3xl font-bold">About</h2>
           <p className=" mb-8 text-lg text-gray-700">
@@ -11,7 +13,8 @@ export default function Page() {
             tennis players at public courts across Philadelphia. Whether
             you&apos;re a seasoned player, burgeoning addict, or just starting
             out, Court Hub makes it easy to find and connect with other players
-            in your &quot;home&quot; court community (and beyond).
+            in your &quot;home&quot; court community, as well as other local
+            courts.
           </p>
           <div className="bg-green-badge  p-8">
             <h3 className="mb-2 text-2xl font-bold ">Our Mission</h3>
@@ -21,16 +24,16 @@ export default function Page() {
               growing tennis community. We believe that tennis should be a
               social and enjoyable experience for everyone. Our platform is
               designed to help players of all levels and experience meet other
-              players who share their preferences and availability.
+              players who share çpreferences and availability.
             </p>
           </div>
         </div>
       </div>
 
       <div className="mb-8 ">
-        <h3 className="mb-4 text-2xl font-bold">How It Works</h3>
-        <div className="flex flex-col space-y-8 p-2 newcard:flex-row newcard:space-x-8 newcard:space-y-0">
-          <div className="border-green-logo flex flex-col rounded-2xl border p-4">
+        <h3 className="z-2 mb-4 text-2xl font-bold">How It Works</h3>
+        <div className="z-2 flex flex-col space-y-8 bg-white/75 p-2 newcard:flex-row newcard:space-x-8 newcard:space-y-0">
+          <div className="z-2 flex  flex-col rounded-2xl border border-gray-400 p-4">
             <OneIcon className="mb-4 mr-auto rounded-full text-2xl " />
             <span className="mb-4 text-lg font-bold">
               Create Your Player Card
@@ -41,7 +44,7 @@ export default function Page() {
               gain access to the Court Hub player directory.
             </p>
           </div>
-          <div className="border-green-logo flex flex-col rounded-2xl border p-4">
+          <div className="z-2 flex  flex-col rounded-2xl border border-gray-400 p-4">
             <TwoIcon className="mb-4 mr-auto rounded-full text-2xl " />
             <span className="mb-4 text-lg font-bold">
               Find Playing Partners
@@ -52,7 +55,7 @@ export default function Page() {
               court or in the area.
             </p>
           </div>
-          <div className="border-green-logo flex flex-col rounded-2xl border p-4">
+          <div className="flex  flex-col rounded-2xl border border-gray-400 p-4">
             <ThreeIcon className="mb-4 mr-auto rounded-full text-2xl " />
             <span className="mb-4 text-lg font-bold">Hit the courts!</span>
             <p className="max-w-md text-gray-700">
@@ -64,22 +67,9 @@ export default function Page() {
         </div>
       </div>
 
-      <div className="mb-8 flex flex-col space-y-4">
-        <h3 className="mb-2 text-2xl font-bold">How We&apos;re Different</h3>
-        <p className="text-lg text-gray-700">
-          Unlike other platforms, Court Hub offers:
-        </p>
-        <span className="font-bold">Free Membership</span> Court Hub is free for
-        all users, with no membership fees.
-        <span className="font-bold">Community Focus</span> We&apos;re focused on
-        connecting players at local courts and fostering a friendly tennis
-        community, rather than solely on competitive league or tournament play--
-        though we have nothing against competing if that&apos;s what you&apos;re
-        interested in!
-        <span className="font-bold">Invite Only</span> Our platform creates a
-        trustworthy community of tennis players because it relies on peer
-        recommendations and is currently operating under an invite-only exposure
-        model.
+      <div className="mb-8">
+        <h3 className="mb-2 text-2xl font-bold">FAQ&apos;s</h3>
+        <Faq />
       </div>
     </div>
   );
---
+++ app/ui/about/faq.tsx
@@ -0,0 +1,74 @@
+'use client';
+import { Disclosure } from '@headlessui/react';
+import { ChevronDownIcon } from '@heroicons/react/20/solid';
+
+export default function Faq() {
+  return (
+    <div className="max-2xl border-green-logo flex w-full flex-col">
+      <Disclosure as="div" className="" defaultOpen={true}>
+        {({ open }) => (
+          <>
+            <Disclosure.Button className="border-green-logo group flex w-full justify-between border-b py-2 text-left">
+              <span className="  font-medium text-blue-600">
+                How is Court Hub different from other tennis community apps?
+              </span>
+              <ChevronDownIcon
+                className={`${
+                  open ? 'rotate-180 transform' : ''
+                } fill-green-logo ui-open:transform  ui-open:rotate-180   h-6 w-6`}
+              />
+            </Disclosure.Button>
+            <Disclosure.Panel className="mt-2 flex flex-col space-y-4">
+              <span className="mt-2 font-bold">Free Membership</span> Court Hub
+              is free for all users, with no membership fees.
+              <span className="font-bold">Community Focus</span> We&apos;re
+              focused on connecting players at local courts and fostering a
+              friendly tennis community, rather than solely on competitive
+              league or tournament play-- though we have nothing against
+              competing if that&apos;s what you&apos;re interested in!
+              <span className="font-bold">Invite Only</span> Our platform
+              creates a trustworthy community of tennis players because it
+              relies on peer recommendations and is currently operating under an
+              invite-only exposure model.
+            </Disclosure.Panel>
+          </>
+        )}
+      </Disclosure>
+      <Disclosure as="div" className="mt-4 ">
+        <Disclosure.Button className="border-green-logo group flex w-full justify-between border-b py-2 text-left">
+          <span className="  font-medium text-blue-600">
+            Can I trust other users on Court Hub?
+          </span>
+          <ChevronDownIcon className=" h-5 w-5" />
+        </Disclosure.Button>
+        <Disclosure.Panel className="mt-2  text-blue-600">
+          Good question. It is always important to be careful about who
+          you&apos;re interacting with and what kind of information you&apos;re
+          sharing online. Court Hub is a platform that relies on peer
+          recommendations and is currently operating under an invite-only
+          exposure model. This means that you can only see and interact with
+          other users who have been invited to the platform by someone who knows
+          someone already on Court Hub. We believe that this system creates a
+          trustworthy community of tennis players who are more likely to be
+          respectful and responsible in their interactions with others. However,
+          it&apos;s always a good idea to exercise caution when meeting new
+          people online, and to be mindful of the information you share about
+          yourself.
+        </Disclosure.Panel>
+      </Disclosure>
+      <Disclosure as="div" className="mt-4 ">
+        <Disclosure.Button className="border-green-logo group flex w-full justify-between border-b py-2 text-left">
+          <span className="  font-medium text-blue-600">
+            Does Court Hub offer any technical support?
+          </span>
+          <ChevronDownIcon className=" h-5 w-5" />
+        </Disclosure.Button>
+        <Disclosure.Panel className="mt-2  text-blue-600">
+          No, at the moment this is just a hobby project by Nathan Paul and is
+          not supported by any kind of developer team. Please reach out with any
+          questions.
+        </Disclosure.Panel>
+      </Disclosure>
+    </div>
+  );
+}
